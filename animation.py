56227e27ff6222ed1c0f67a79dfe8aa6136d59ab
Vince Hintermeister <vv.hintermeister@gmail.com>
2021-03-11 18:28:54 -0600

    added cubes via loop

--- animation.py
+++ animation.py
@@ -1,3 +1,15 @@
 #this is my first script in blender yay!
 import bpy
-bpy.ops.mesh.primitive_monkey_add(enter_editmode=False, align='WORLD', location=(5, 10, 0), scale=(1, 1, 1))
+from mathutils import Vector
+
+pos = Vector((0.5, 0.5, 0.5))
+init_x = pos.x
+init_y = pos.y
+base = 4
+
+for xcubes in range(base):
+    bpy.ops.mesh.primitive_cube_add(size = 1, location = pos)
+    pos.x += 1
+    
+pos.x = init_x
+
