3e654511de2c0022169a86b23c4bdc1ef7d97dc9
Vince Hintermeister <vv.hintermeister@gmail.com>
2021-03-15 15:42:45 -0500

    changes to addobject menu

--- AddObject.py
+++ AddObject.py
@@ -1,4 +1,23 @@
+bl_info = {
+    "name" : "Object Addon",
+    "author" : "Vince",
+    "version" : (1,0),
+    "blender" : (2,92,0),
+    "location" : "View3d > Tool",
+    "warning" : "",
+    "wikie_url" : "",
+    "category" : "Add Mesh",
+    
+}
+
+
 import bpy
+
+
+
+
+
+
 
 class TestPanel(bpy.types.Panel):
     bl_label = "Test Panel"
@@ -6,6 +25,8 @@ class TestPanel(bpy.types.Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "My 1st Addon"
+    
+    bl_options = {'DEFAULT_CLOSED'}
     
     def draw(self, context):
         layout = self.layout
@@ -16,14 +37,54 @@ class TestPanel(bpy.types.Panel):
         row.operator("mesh.primitive_cube_add", icon= 'CUBE')   
         row = layout.row() 
         row.operator("mesh.primitive_uv_sphere_add", icon= "SPHERE") 
-        row = layour.row()
-        row.operator("object.text_add")
+        row = layout.row()
+        row.operator("object.text_add", icon= 'FILE_FONT', text = "Font Button")
+        
+        
+class PanelA(bpy.types.Panel):
+    bl_label = "Scale"
+    bl_idname = "PT_PanelA"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "My 1st Addon"
+    bl_parent_id = "PT_TestPanel"
+    bl_options = {'DEFAULT_CLOSED'}
+    
+    def draw(self, context):
+        layout = self.layout
+        obj = context.object
+        row = layout.row()
+        row.label(text= "Select an option to scale your object.", icon= "FONT_DATA")
+        row = layout.row()
+        row.operator("transform.resize")
+        row = layout.row()
+        row.prop(obj, "scale")
+        
+        
+class PanelB(bpy.types.Panel):
+    bl_label = "PanelB"
+    bl_idname = "PT_PanelB"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "My 1st Addon"
+    bl_parent_id = "PT_TestPanel"
+    bl_options = {'DEFAULT_CLOSED'}
+    
+    def draw(self, context):
+        layout = self.layout
+        
+        row = layout.row()
+        row.label(text= "This Is Panel B", icon= "FONT_DATA")
         
 def register():
     bpy.utils.register_class(TestPanel)
+    bpy.utils.register_class(PanelA)
+    bpy.utils.register_class(PanelB)
 
 def unregister():
     bpy.utils.unregister_class(TestPanel)
+    bpy.utils.unregister_class(PanelA)
+    bpy.utils.unregister_class(PanelB)
     
 if __name__ == "__main__":
     register()
