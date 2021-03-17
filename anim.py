2506ee8c0637d406e7bb8db3b3e4ad49b8a47eff
Vince Hintermeister <vv.hintermeister@gmail.com>
2021-03-17 18:37:51 -0500

    The ball fully works and has some more movement.

--- anim.py
+++ anim.py
@@ -1,13 +1,14 @@
 import bpy
 
-positions = (0, 3, 2), (4, 3, 6), (3, 5, 1), (3, 20, 1), (4, 7, 2)
-start_pos = (0, 0, 0)
+positions = (0, 0, 0), (0, 3, 2), (4, 3, 6), (3, 5, 4), (7, -1, 3), (3, 20, 7), (4, 7, 2), (0, 3, 2), (0, 0, 0)
+#start_pos = (0, 0, 0)
 
 ob = bpy.data.objects["Sphere"]
-ob.location = start_pos
+#ob.location = start_pos
 frame_num = 0
 
 for position in positions:
+#    ob.location = start_pos
     bpy.context.scene.frame_set(frame_num)
     ob.location = position
     ob.keyframe_insert(data_path="location", frame=frame_num)
